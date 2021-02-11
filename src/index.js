025e93e2aae3840e53fbf8efcdf0edbb34690dda
Siarhei Khonski <armonius@mail.ru>
2021-02-11 23:41:07 +0300

    refactor: improve productivity

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
-  while (search()) del();
-    
   
-  function search() {
-    for (i = 0; i < bracketsConfig.length; i++) if (str.includes(bracketsConfig[i].join(''))) return true;
+  while (searchDel());
+   
+  function searchDel() {
+    for (i = 0; i < bracketsConfig.length; i++) if (str.includes(bracketsConfig[i].join(''))) 
+    {{while (str.includes(bracketsConfig[i].join(''))) 
+    str = str.replace(bracketsConfig[i].join(''), '')}
+    return true}
     return false;
   }
-  
-  function del() {
-  bracketsConfig.forEach(val => {while (str.includes(val.join(''))) 
-                                 str = str.replace(val.join(''), '')}); console.log(str)}
-  
+    
   return str.length ? false : true;
 }
