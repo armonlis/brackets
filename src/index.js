35cff665fec1b59d48b43495f730a2c83622366f
Siarhei Khonski <armonius@mail.ru>
2021-02-11 22:30:53 +0300

    fix: fix index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  bracketsConfig.forEach(val => {while (str.includes(val.join(''))) {
-                                 str = str.replace(val.join(''), '')}});
+  while (search()) del();
+    
+  
+  function search() {
+    for (i = 0; i < bracketsConfig.length; i++) if (str.includes(bracketsConfig[i].join(''))) return true;
+    return false;
+  }
+  
+  function del() {
+  bracketsConfig.forEach(val => {while (str.includes(val.join(''))) 
+                                 str = str.replace(val.join(''), '')}); console.log(str)}
+  
   return str.length ? false : true;
 }
